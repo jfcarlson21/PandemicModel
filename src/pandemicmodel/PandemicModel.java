91d90d89517a2d4e28a7446e92ce7d9240f1ef86
jakecarlson <jcarlson21@taboracademy.org>
2021-01-22 08:40:16 -0500

    initial commit

---
+++ src/pandemicmodel/PandemicModel.java
@@ -0,0 +1,21 @@
+/*
+ * Jake Carlson
+ * AT Comp Sci 2020-2021
+ * 
+ */
+package pandemicmodel;
+
+/**
+ *
+ * @author jakecarlson
+ */
+public class PandemicModel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
