e42469f1e5ae65ec3cab5c634c66d3c5bd7788b1
ecederholm21 <71302798+ecederholm21@users.noreply.github.com>
2021-01-26 21:57:25 -0500

    Line Basics

--- src/pandemicmodel/Line.java
+++ src/pandemicmodel/Line.java
@@ -5,10 +5,37 @@
  */
 package pandemicmodel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lainiecederholm
  */
 public class Line {
+    
+    static ArrayList<Integer> line = new ArrayList<>();
+    static ArrayList<Integer> nextLoc = new ArrayList<>();
+    
+    public static void lineTest(){
+        for(int i=1; i<=10; i++){
+            line.add(i);
+        }
+    }
+    
+    public static void leaveLine(){
+        Line.lineTest();
+        if(line.size()>0){
+            for(int i=0; i<10; i++){
+                nextLoc.add(line.get(0));
+                line.remove(0);
+                System.out.println(nextLoc);
+                System.out.println(line);
+            }
+            
+        }
+        else{
+            System.out.println("empty line");
+        }
+    }
     
 }
--- src/pandemicmodel/PandemicModel.java
+++ src/pandemicmodel/PandemicModel.java
@@ -16,6 +16,7 @@ public class PandemicModel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
