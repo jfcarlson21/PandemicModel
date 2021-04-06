b3b7dd8cfee436b582cb3a102b3beff95c61c9eb
jakecarlson <jcarlson21@taboracademy.org>
2021-04-06 09:03:47 -0400

    added physical ride and cart class

---
+++ src/pandemicmodel/Cart.java
@@ -0,0 +1,7 @@
+package pandemicmodel;
+
+public class Cart extends java.util.ArrayList<Surfaces>{
+    public Cart(int seats){
+        this.add(new Surfaces("metal"));
+    }
+}
--- src/pandemicmodel/Ride.java
+++ src/pandemicmodel/Ride.java
@@ -5,10 +5,26 @@
  */
 package pandemicmodel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lainiecederholm
  */
 public class Ride {
-    
+    int seatsPerCart;
+    int numOfCarts;
+    public Ride(int cartNum, int seatsOneCart) {
+        seatsPerCart = seatsOneCart;
+        numOfCarts = cartNum;
+        ArrayList<Cart> fullRide = new ArrayList<Cart>();
+        for (int i = 0; i < numOfCarts; i++) {
+            Cart cartE = new Cart(seatsPerCart);
+            fullRide.add(cartE);
+        }
+    }
+
+    public void takePassengers(){
+
+    }
 }
