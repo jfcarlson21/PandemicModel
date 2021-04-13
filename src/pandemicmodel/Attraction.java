0b2d15f457de8c1109b8b3339b36d17eda7b6ba5
jakecarlson <jcarlson21@taboracademy.org>
2021-04-13 09:10:03 -0400

    lots of stuff

--- src/pandemicmodel/Attraction.java
+++ src/pandemicmodel/Attraction.java
@@ -10,19 +10,4 @@ public class Attraction {
     public Attraction(Point loc){
         location = loc;
     }
-    public class Ride{
-        public int seatsPerCart;
-        public int numOfCarts;
-        public Ride(int cartNum, int seatsOneCart) {
-
-            seatsPerCart = seatsOneCart;
-            numOfCarts = cartNum;
-            leaveRate = seatsOneCart;
-            ArrayList<Cart> fullRide = new ArrayList<Cart>();
-            for (int i = 0; i < numOfCarts; i++) {
-                Cart cartE = new Cart(seatsPerCart);
-                fullRide.add(cartE);
-            }
-        }
-    }
 }
