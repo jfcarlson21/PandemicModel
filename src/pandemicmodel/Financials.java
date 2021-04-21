a9b3e7401dbe02237a92f79c13c036b66af4eb7a
ecederholm21 <71302798+ecederholm21@users.noreply.github.com>
2021-04-21 10:32:07 -0400

    filewriter

--- src/pandemicmodel/Financials.java
+++ src/pandemicmodel/Financials.java
@@ -7,6 +7,7 @@ package pandemicmodel;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -14,7 +15,7 @@ import java.util.Scanner;
  * @author lainiecederholm
  */
 public class Financials {
-    public Financials(String fileName, int capacity, int employee){
+    public Financials(String fileName, double capacity, int employee){
         Scanner fileIn = null;
         double minWage, ticketPrice, totTicketRev, hours;
         double priceRags, priceBleach, employeeWage, totExpenses;
@@ -39,8 +40,21 @@ public class Financials {
         priceRags = employee*100*50/250;
         priceBleach = employee*60/8;
         totExpenses = employeeWage+priceRags+priceBleach;
-        
         totTicketRev = capacity*maxCapacity*ticketPrice;
+        
+        try{
+                java.io.FileWriter mW = new java.io.FileWriter("financeOutput.txt");
+                mW.write(Double.toString(totExpenses));
+                mW.write("\n");
+                mW.write(Double.toString(totTicketRev));
+                mW.close();
+            }
+        
+            catch(IOException e){
+                System.out.println("error");
+                e.printStackTrace();
+            }
+        
         
     }
     
