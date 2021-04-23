80138e20769197fcbb66154c42fb96ebe56e9303
ecederholm21 <71302798+ecederholm21@users.noreply.github.com>
2021-04-22 21:55:49 -0400

    gui outputs

--- src/pandemicmodel/Financials.java
+++ src/pandemicmodel/Financials.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  *
  * @author lainiecederholm
  */
+        
 public class Financials {
     public Financials(gui inputs){
         Scanner fileIn = null;
--- src/pandemicmodel/gui.java
+++ src/pandemicmodel/gui.java
@@ -6,6 +6,7 @@
 package pandemicmodel;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.*;
 /**
  *
@@ -21,6 +22,8 @@ public class gui {
     public double minWage;
     public double ticketPrice;
     public double hours;
+    public ArrayList<String> stringList = new ArrayList<>();
+    public ArrayList<Integer> intList = new ArrayList<>();
             
     public void parkInfoGUI() throws IOException{
         JPanel generalParkPanel = new JPanel();
@@ -77,6 +80,63 @@ public class gui {
     }
     
     public void outputGui(){
+        JPanel outputPanel1 = new JPanel();
+        GridLayout layout = new GridLayout(0,3);
+        outputPanel1.setLayout(layout);
+        JLabel rideName = new JLabel("Name of Ride");
+        JLabel infectNum = new JLabel("People Infected at Ride");
+        JLabel sanitationStatus = new JLabel("Sanitation Status");
+        outputPanel1.add(rideName);
+        outputPanel1.add(infectNum);
+        outputPanel1.add(sanitationStatus);
+        
+        while(!stringList.isEmpty()&& !intList.isEmpty()){
+            JLabel ride = new JLabel(stringList.get(0));
+            JLabel infNum = new JLabel(Integer.toString(intList.get(0)));
+            outputPanel1.add(ride);
+            outputPanel1.add(infNum);
+            stringList.remove(0);
+            intList.remove(0);    
+        }
+        int result = JOptionPane.showConfirmDialog(null, outputPanel1, 
+                    "Ride Specific Data", JOptionPane.CLOSED_OPTION);
+        
+        JPanel outputPanel2 = new JPanel();
+        GridLayout layout2 = new GridLayout(0,2);
+        outputPanel2.setLayout(layout2);
+        JLabel tePrompt = new JLabel("Total Added Expenses");
+        JLabel trPrompt = new JLabel("Total Ticket Revenue");
+        JLabel tcPrompt = new JLabel("Total Capacity");
+        JLabel iiPrompt = new JLabel("Initial Number of Infected");
+        JLabel tiPrompt = new JLabel("Final Number of Infected");
+        
+        double employeeWage = employeeNumber*minWage*hours;
+        int totCap = (int)((int)maxCapacity*percentCapacity);
+        int totInf = (int)(maxCapacity*percentInfected);
+        double priceRags = employeeNumber*100*50/250;
+        double priceBleach = employeeNumber*60/8;
+        double totExpenses = employeeWage+priceRags+priceBleach;
+        double totTicketRev = percentCapacity*maxCapacity*ticketPrice;
+        
+        JLabel te = new JLabel(Double.toString(totExpenses));
+        JLabel tr = new JLabel(Double.toString(totTicketRev));
+        JLabel tc = new JLabel(Integer.toString(totCap));
+        JLabel ii = new JLabel(Integer.toString(totInf));
+        // how to get number of infected people
+        JLabel ti = new JLabel();
+        
+        outputPanel2.add(tcPrompt);
+        outputPanel2.add(tc);
+        outputPanel2.add(iiPrompt);
+        outputPanel2.add(ii);
+        outputPanel2.add(tiPrompt);
+        outputPanel2.add(ti);
+        outputPanel2.add(tePrompt);
+        outputPanel2.add(te);
+        outputPanel2.add(trPrompt);
+        outputPanel2.add(tr);
+        int result2 = JOptionPane.showConfirmDialog(null, outputPanel2, 
+                    "Park Results", JOptionPane.CLOSED_OPTION);
         
     }
         
